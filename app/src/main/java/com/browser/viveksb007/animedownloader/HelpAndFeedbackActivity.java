7d3c8e49f5c598d77c4963a48089b0bffdf62610
abelqwerty <shemar268abel@gmail.com>
2020-03-27 13:52:13 -0400

    Description edit

--- app/src/main/java/com/browser/viveksb007/animedownloader/HelpAndFeedbackActivity.java
+++ app/src/main/java/com/browser/viveksb007/animedownloader/HelpAndFeedbackActivity.java
@@ -3,7 +3,7 @@ package com.browser.viveksb007.animedownloader;
 import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by viveksb007 on 3/10/17.
+ * Created by viveksb007 on 3/10/17 Forked by Abel-S on 03/27/20.
  */
 
 public class HelpAndFeedbackActivity extends AppCompatActivity {
